e32d38875cda3344f07a23d307d9a20c64ecd1dd
RR <RubenR42@icloud.com>
2020-04-29 12:35:39 -0700

    data.js adds 6 additional apps

--- src/data.js
+++ src/data.js
@@ -1,9 +1,15 @@
 const apps = [
-    { name: 'Slader', url: 'https://apps.apple.com/us/app/slader-homework-answers/id579962583' },
-    { name: 'Quizlet', url: 'https://apps.apple.com/us/app/quizlet/id546473125' },
-    { name: 'Photomath', url: 'https://apps.apple.com/us/app/photomath/id919087726' },
-    { name: 'ABCya', url: 'https://apps.apple.com/us/app/abcya-games/id1079974015' },
-    { name: 'Khan Academy', url: 'https://apps.apple.com/us/app/khan-academy/id469863705' },
+    { name: 'Slader', url: 'https://apps.apple.com/us/app/slader-homework-answers/id579962583', img: 'https://is3-ssl.mzstatic.com/image/thumb/Purple113/v4/05/8f/19/058f19f0-a060-dd89-8471-ab1a51486521/AppIcon-0-0-1x_U007emarketing-0-0-0-7-0-0-85-220.jpeg/230x0w.jpg' },
+    { name: 'Quizlet', url: 'https://apps.apple.com/us/app/quizlet/id546473125', img: 'https://is4-ssl.mzstatic.com/image/thumb/Purple123/v4/da/f5/cf/daf5cf0f-b58c-6511-3e9a-1b36a82604ff/AppIcon-production-0-0-1x_U007emarketing-0-0-0-7-0-0-sRGB-0-0-0-GLES2_U002c0-512MB-85-220-0-0.png/230x0w.png' },
+    { name: 'Photomath', url: 'https://apps.apple.com/us/app/photomath/id919087726', img: 'https://is3-ssl.mzstatic.com/image/thumb/Purple113/v4/1e/99/e3/1e99e381-df49-9341-c756-4d11f8047786/AppIcon-1x_U007emarketing-0-7-0-0-85-220.png/230x0w.png' },
+    { name: 'ABCya', url: 'https://apps.apple.com/us/app/abcya-games/id1079974015', img: 'https://is1-ssl.mzstatic.com/image/thumb/Purple113/v4/2c/84/fe/2c84fe0c-52a6-7ff3-47a5-060475c121ae/AppIcon-0-0-1x_U007emarketing-0-0-0-7-0-85-220.png/230x0w.png' },
+    { name: 'Khan Academy', url: 'https://apps.apple.com/us/app/khan-academy/id469863705', img: 'https://is3-ssl.mzstatic.com/image/thumb/Purple113/v4/06/91/6c/06916c3e-722b-d2f8-4c9f-9199cf043eb1/AppIcon-0-0-1x_U007emarketing-0-0-0-7-0-0-sRGB-0-0-0-GLES2_U002c0-512MB-85-220-0-0.png/230x0w.png' },
+    { name: 'Actively Learn', url: 'https://apps.apple.com/us/app/actively-learn/id1366111784', img: 'https://is4-ssl.mzstatic.com/image/thumb/Purple128/v4/52/93/bc/5293bccd-7bf1-7886-387b-23f1c2cc7e97/AppIcon-1x_U007emarketing-0-0-GLES2_U002c0-512MB-sRGB-0-0-0-85-220-0-0-0-5.png/230x0w.png' },
+    { name: 'Achieve3000', url: 'https://apps.apple.com/us/app/achieve3000/id757933892', img: 'https://is4-ssl.mzstatic.com/image/thumb/Purple113/v4/06/29/f2/0629f299-75b2-35cc-5923-810f0eead3d7/AppIcon-0-1x_U007emarketing-0-85-220-0-10.png/230x0w.png' },
+    { name: 'Writable', url: 'https://www.writable.com/', img: 'https://1wtywzj54kj2qxsdu4aufiil-wpengine.netdna-ssl.com/wp-content/uploads/2019/02/writable_logo.png' },
+    { name: 'Mozi ', url: 'https://www.themozi.com/', img: 'https://www.themozi.com/mozi_white_transparent.png' },
+    { name: 'Google Classroom ', url: 'https://classroom.google.com/', img: 'https://upload.wikimedia.org/wikipedia/commons/5/59/Google_Classroom_Logo.png' },
+    { name: 'ZOOM', url: 'https://apps.apple.com/us/app/zoom-cloud-meetings/id546505307', img: 'https://is2-ssl.mzstatic.com/image/thumb/Purple113/v4/e3/4b/a6/e34ba6f3-be8a-b36e-227a-e21c8b119154/AppIcon-0-1x_U007emarketing-0-0-85-220-9.png/217x0w.png' },
 ]
 
 export default apps;
