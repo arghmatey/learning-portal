061db61a9a1d6d8a5ca2c3543b30f260fb257d8e
Sarah <40401149+arghmatey@users.noreply.github.com>
2020-04-30 14:55:19 -0700

    add favorite on app page

--- src/pages/AppPage/AppPage.jsx
+++ src/pages/AppPage/AppPage.jsx
@@ -6,17 +6,31 @@ class AppPage extends Component {
     render() {
         return (
             <div>
-                <div>This is where the search bar will be</div>
                 <h2>Put on your thinking app</h2>
                 <div>
                     <h3>Your Apps</h3>
-                </div>
-                <div>
-                    <h3>Apps we think you'll like</h3>
-                    <AppList
-                        user={this.props.user}
-                        favApps={this.props.favApps}
-                        handleFavApp={this.props.handleFavApp} />
+                    <div> {
+                        this.props.favApps.length > 0 ?
+                            this.props.favApps.map(app =>
+                                <>
+                                    <a href={app.url}>
+                                        <img alt={app.name} src={app.img} className="appButton" />
+                                    </a>
+                                    {app.name}
+                                </>
+                            )
+                            : <div>add an app</div>}
+                    </div>
+                    <div>
+                        <h3>Your Apps</h3>
+                    </div>
+                    <div>
+                        <h3>Apps we think you'll like</h3>
+                        <AppList
+                            user={this.props.user}
+                            favApps={this.props.favApps}
+                            handleFavApp={this.props.handleFavApp} />
+                    </div>
                 </div>
             </div>
         );
