b2d0f6b357f205db4e823f57055510f57bcd628b
Sarah <40401149+arghmatey@users.noreply.github.com>
2021-06-11 16:33:11 -0700

    remove font import

--- src/index.js
+++ src/index.js
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './pages/App/App';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-// import './fonts/AvenirNext-Regular.ttf';
-// import './fonts/AvenirNext-Medium.ttf';
-// import './fonts/AvenirNext-Bold.ttf';
 
 ReactDOM.render(
   <Router><Route render={({ history }) => <App history={history} />} /> </Router>,
