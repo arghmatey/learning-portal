2af72dabdc9ce1a8b22e7a9976341f852a59f0f0
Sarah <40401149+arghmatey@users.noreply.github.com>
2021-06-15 12:06:46 -0700

    resize logo on navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -3,61 +3,50 @@ import './NavBar.css';
 import { Link } from 'react-router-dom';
 
 const NavBar = (props) => {
-    let nav = props.user ?
-            <header className="App-header">
-                <div className="nav-app-logo">
-                    <img alt="Grasp Logo" src="./icons/Logo.png" />
-                </div>
-                <div className="nav-links-wrapper">
-                    <div className="nav-link">
-                        <div className="active-page-indicator"></div>
-                        <Link to='/'>
-                            <img alt="Homepage icon" className="nav-link-icon" src="./icons/home.png" />
-                            <span>Home</span>
-                        </Link>
-                    </div>
-                    <div className="nav-link">
-                        <div className="active-page-indicator"></div>
-                        <Link to='/apps'>
-                            <img alt="Apps page icon" className="nav-link-icon" src="./icons/apps.png" />
-                            <span>Applications</span>
-                        </Link>
-                    </div>
-                    <div className="nav-link">
-                        <div className="active-page-indicator"></div>
-                        <Link to='/progress'>
-                            <img alt="Progress page icon" className="nav-link-icon" src="./icons/chart-line.png" />
-                            <span>Progress</span>
-                        </Link>
-                    </div>
-                    <div className="nav-link">
-                        <div className="active-page-indicator"></div>
-                        <Link to='/chat'>
-                            <img alt="Chat page icon" className="nav-link-icon" src="./icons/forum.png" />
-                            <span>Chat</span>
-                        </Link>
-                    </div>
-                </div>
-                <div className="nav-logout-link">
-                    <div className="nav-link">
-                        <Link to='' onClick={props.handleLogout}>
-                            <img alt="Logout icon" className="nav-link-icon" src="./icons/logout.png" />
-                            <span>Logout</span>
-                        </Link>
-                    </div>
-                </div>
-            </header>
-        :
-        <div>
-            <Link to='/login' className="neon-nav">Login</Link>
-            &nbsp;&nbsp;|&nbsp;&nbsp;
-            <Link to='/signup' className="neon-nav">Signup</Link>
-        </div>;
-
     return (
-        <div>
-            {nav}
-        </div>
+        <header className="App-header">
+            <div className="nav-app-logo">
+            <img style={{height: "110px"}} alt="Grasp Logo" src="./icons/Logo.png" />
+            </div>
+            <div className="nav-links-wrapper">
+                <div className="nav-link">
+                    <div className="active-page-indicator"></div>
+                    <Link to='/'>
+                        <img alt="Homepage icon" className="nav-link-icon" src="./icons/home.png" />
+                        <span>Home</span>
+                    </Link>
+                </div>
+                <div className="nav-link">
+                    <div className="active-page-indicator"></div>
+                    <Link to='/apps'>
+                        <img alt="Apps page icon" className="nav-link-icon" src="./icons/apps.png" />
+                        <span>Applications</span>
+                    </Link>
+                </div>
+                <div className="nav-link">
+                    <div className="active-page-indicator"></div>
+                    <Link to='/progress'>
+                        <img alt="Progress page icon" className="nav-link-icon" src="./icons/chart-line.png" />
+                        <span>Progress</span>
+                    </Link>
+                </div>
+                <div className="nav-link">
+                    <div className="active-page-indicator"></div>
+                    <Link to='/chat'>
+                        <img alt="Chat page icon" className="nav-link-icon" src="./icons/forum.png" />
+                        <span>Chat</span>
+                    </Link>
+                </div>
+            </div>
+            <div className="nav-logout-link">
+                <div className="nav-link">
+                    <Link to='' onClick={props.handleLogout}>
+                        <img alt="Logout icon" className="nav-link-icon" src="./icons/logout.png" />
+                        <span>Logout</span>
+                    </Link>
+                </div>
+            </div>
+        </header>
     );
 };
 
