d118d29ac0f0d22ab651fcda7280472fad39acd0
Sarah <40401149+arghmatey@users.noreply.github.com>
2021-06-15 12:07:33 -0700

    remove appbutton from app info component

--- src/components/AppButton/AppButton.jsx
+++ src/components/AppButton/AppButton.jsx
@@ -6,11 +6,7 @@ export default class AppButton extends Component {
             <div className="appButton">
                 <>
 
-            {this.props.name}
-                    <a href={this.props.url}>
-
-                        <img alt={this.props.name} src={this.props.img} className="appButton" />
-                    </a>
+            
                 </>
             </div>
         )
--- src/components/AppList/AppList.jsx
+++ src/components/AppList/AppList.jsx
@@ -1,29 +1,19 @@
 import React, { Component } from 'react';
-import AppButton from "../AppButton/AppButton"
 import apps from "../../utils/data.js"
 
 export default class AppList extends Component {
 
     render() {
         return (
-            <div className="appList">
+            <div className="application-info">
                 {apps.map(app =>
-                    <>
-                        <AppButton
-                            url={app.url}
-                            img={app.img}
-                            name={app.name}
-                        />
-                        <button style={{"height" : "20%", "borderRadius": "20%", "fontSize" : "10px", "margin" : "1.5% .2% 1% 0%"}} onClick={() => this.props.handleFavApp({
-                            favoritedBy: this.props.user,
-                            name: app.name,
-                            url: app.url,
-                            img: app.img
-                        })}>Add to Favs
-                    </button>
-                    </>
-                )
-                }
+                    <div className="application-details">
+                        <a href={app.url}>
+                            <img alt={app.name} src={app.img} className="appButton" />
+                        </a>
+                        <div>{app.name}</div>
+                    </div>
+                )}
             </div>
         )
     }
