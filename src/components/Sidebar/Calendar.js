80354b0da98cffd91edd23319d99ee7aa25cd150
Sarah <40401149+arghmatey@users.noreply.github.com>
2021-06-16 01:08:39 -0700

    create calendar component;

---
+++ src/components/Sidebar/Calendar.js
@@ -0,0 +1,42 @@
+// This component uses React Hooks. 
+
+import React, { useState, useEffect } from 'react';
+
+const Calendar = () => {
+    const [date, setDate] = useState('');
+
+    useEffect(() => {
+        var today = new Date;
+        console.log(today.toLocaleDateString(undefined, { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' }));
+    })
+
+    return (
+        <div className="calendar">
+            <div className="date-today">June 7, 2021</div>
+            <div className="days">
+                <div className="weekday">
+                <div className="day-letter">M</div>
+                <div className="day-number">1</div>
+                </div>
+                <div className="weekday">
+                <div className="day-letter">Tu</div>
+                <div className="day-number">2</div>
+                </div>
+                <div className="weekday">
+                <div className="day-letter">W</div>
+                <div className="day-number">3</div>
+                </div>
+                <div className="weekday">
+                <div className="day-letter">Th</div>
+                <div className="day-number">4</div>
+                </div>
+                <div className="weekday">
+                <div className="day-letter">F</div>
+                <div className="day-number">5</div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Calendar;
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,61 +1,22 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import TodoList from "../TodoList/TodoList"
+import TodoList from '../TodoList/TodoList';
+import Calendar from './Calendar';
 import './Sidebar.css';
 
 const Sidebar = (props) => {
-    let sidebar = props.user ?
+
+    return (
         <div className="sidebar-wrapper">
             <div className="user-wrapper">
                 <img alt="User profile avatar" className="nav-link-icon" src="./icons/Avatar.png" />
                 <span>{props.user.name}</span>
             </div>
 
-
-            <div className="calendar">
-                <div className="date-today">June 7, 2021</div>
-                <div className="days">
-                    <div className="weekday">
-                    <div className="day-letter">M</div>
-                    <div className="day-number">1</div>
-                    </div>
-                    <div className="weekday">
-                    <div className="day-letter">Tu</div>
-                    <div className="day-number">2</div>
-                    </div>
-                    <div className="weekday">
-                    <div className="day-letter">W</div>
-                    <div className="day-number">3</div>
-                    </div>
-                    <div className="weekday">
-                    <div className="day-letter">Th</div>
-                    <div className="day-number">4</div>
-                    </div>
-                    <div className="weekday">
-                    <div className="day-letter">F</div>
-                    <div className="day-number">5</div>
-                    </div>
-                </div>
-
-
-            </div>
-
+            <Calendar />
 
             <div className="todo-wrapper">
-                
                 <TodoList/>
             </div>
-        </div>
-        :
-        <div>
-            <Link to='/login'>Login</Link>
-            &nbsp;&nbsp;|&nbsp;&nbsp;
-            <Link to='/register'>Register</Link>
-        </div>;
-
-    return (
-        <div>
-            {sidebar}
         </div>
     );
 };
