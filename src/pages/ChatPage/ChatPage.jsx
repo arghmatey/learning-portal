539aa4a48e25ddf3a75f122a0ca75b69ca6e1086
Sarah <40401149+arghmatey@users.noreply.github.com>
2021-07-12 18:22:08 -0700

    add chat window component

--- src/pages/ChatPage/ChatPage.jsx
+++ src/pages/ChatPage/ChatPage.jsx
@@ -1,15 +1,60 @@
-import React, { Component } from 'react';
-import './ChatPage.css';
+// This page uses React Hooks
 
+import React, { useEffect, useState } from 'react';
 
-class ChatPage extends Component {
-    render() {
-        return (
-           <div>
-               <img id='dashboard' alt="" src="./icons/pics/chat.png" />
-           </div>
-        );
-    }
+const ChatPage = () => {
+
+    return (
+
+        <div className="chatWrapper">
+            <div className="chatWindow">
+                <div className="messagesWrapper">
+                    {/* {props.messages.map((message, idx) => (
+                        props.username === message.user
+                            ?
+                            <div key={idx} className="userMessage" >
+                                <h4>{message.user}</h4>
+                                <div>{message.text}</div>
+                            </div>
+                            :
+                            <div key={idx} className="chatMessage">
+                                <h4>{message.user}</h4>
+                                <div>{message.text}</div>
+                            </div>
+                    ))} */}
+                </div >
+
+                <div className="usersWrapper">
+                    <div>
+                    <h3>ROOM NAMES</h3>
+                    <h3>
+                        DMs HERE
+                        {/* {props.users.map(user => (
+                            <div key={user.id}>{user.username}</div>
+                        ))} */}
+                    </h3>
+                    </div>
+                </div>
+
+                <div className='newWrapper'>
+                    <input
+                        placeholder='Type your message here...'
+                        className='newMessage'
+                        // value={props.message}
+                        // onChange={e => props.setMessage(e.target.value)}
+                        // onKeyPress={e => e.key === 'Enter' ? props.sendMessage(e) : null} 
+                    />
+                    <button
+                        className='messageSend'
+                        type='submit'
+                        // onClick={e => props.sendMessage(e)}
+                        >
+                        Send
+                    </button>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default ChatPage;
